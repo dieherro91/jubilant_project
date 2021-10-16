44969a4080b6fc0b2f76b4303ba81b3f3928a24b
dieherro91 <dihromeroro@unal.edu.co>
2021-10-15 21:27:03 -0500

    cambio de bootstrap

--- Sprint_3/sicabulls_react/src/components/Productos/ControlProductos.js
+++ Sprint_3/sicabulls_react/src/components/Productos/ControlProductos.js
@@ -7,37 +7,39 @@ import { Tooltip } from '@material-ui/core';
 import PrivateRoute from '../Login/PrivateRoute.js'
 
 function ControlProductos() {
-    
+
     const [active, setActive] = useState("buscar_Producto");
-    
-    return  (
+
+    return (
         <PrivateRoute>
             <div>
-                <div className="container" id="titulo_funcion_Productos">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <h2 id="titulo_app_Productos">Registro de servicios</h2>
+                <div className="container" >
+                    <div className="card shadow-lg p-3 mb-5 bg-white rounded" id="titulo_funcion_Productos">
+                        <div className="card-body">
+                            <div className="row justify-content-between">
+                                <div className="col-md-4">
+                                    <h2 id="titulo_app_Productos">Registro de servicios</h2>
+                                </div>
+                                <div className="col-md-4">
+                                    <nav id="nav_buttons_Productos">
+                                        <Tooltip title="App de busqueda" arrow>
+                                            <button onClick={() => setActive("buscar_Producto")} type="button" className="btn btn-primary">buscar</button>
+                                        </Tooltip>
+                                        <Tooltip title="App de edición" arrow>
+                                            <button onClick={() => setActive("actualizar_Producto")} type="button" className="btn btn-primary">actualizar</button>
+                                        </Tooltip>
+                                    </nav>
+                                </div>
+                            </div>
+                            <div className="row">
+                                {active === "buscar_Producto" && <h4>Buscar: </h4>}
+                                {active === "actualizar_Producto" && <h4>Actualizar: </h4>}
+                            </div>
                         </div>
-                        <div className="col-md-4 offset-md-4">
-                            <nav id="nav_buttons_Productos">
-                                <Tooltip title="App de busqueda" arrow>
-                                    <button onClick={() => setActive("buscar_Producto")} type="button" className="btn btn-primary">buscar</button>
-                                </Tooltip>
-                                <Tooltip title="App de edición" arrow>
-                                    <button onClick={() => setActive("actualizar_Producto")} type="button" className="btn btn-primary">actualizar</button>
-                                </Tooltip>
-                                
-                            </nav>
-                        </div>
-                    </div>
-                    <div className="row">
-                        {active === "buscar_Producto" && <h4>Buscar: </h4>}
-                        {active === "actualizar_Producto" && <h4>Actualizar: </h4>}
                     </div>
                 </div>
-
                 <div className="container">
-                    <div className="card" id="table_display_Productos">
+                    <div className="card shadow-lg p-3 mb-5 bg-white rounded" id="table_display_Productos">
                         <div className="card-body">
                             <div>
                                 {active === "buscar_Producto" && <TableProductoSearch></TableProductoSearch>}
@@ -48,8 +50,8 @@ function ControlProductos() {
 
                 </div>
             </div>
-            </PrivateRoute>
-        )
+        </PrivateRoute>
+    )
 
 }
 
--- Sprint_3/sicabulls_react/src/components/Ventas/ControlVentas.js
+++ Sprint_3/sicabulls_react/src/components/Ventas/ControlVentas.js
@@ -13,31 +13,37 @@ function ControlVentas() {
     return (
         <PrivateRoute>
             <div>
-                <div className="container" id="titulo_funcion_Ventas">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <h2 id="titulo_app_Ventas">Registro de Ventas</h2>
-                        </div>
-                        <div className="col-md-4 offset-md-4">
-                            <nav id="nav_buttons_Ventas">
-                                <Tooltip title="App de busqueda" arrow>
-                                    <button onClick={() => setActive("buscar_Venta")} type="button" className="btn btn-primary">buscar</button>
-                                </Tooltip>
-                                <Tooltip title="App de edición" arrow>
-                                    <button onClick={() => setActive("actualizar_Venta")} type="button" className="btn btn-primary">actualizar</button>
-                                </Tooltip>
+                <div className="container" >
+                    <div className="card shadow-lg p-3 mb-5 bg-white rounded" id="titulo_funcion_Ventas">
+                        <div className="card-body">
+                            <div className="row justify-content-between">
+                                <div className="col">
+                                    <h2 id="titulo_app_Ventas">Registro de Ventas</h2>
+                                </div>
+                                <div className="col">
+                                    <nav className="navbar navbar-expand-lg navbar-light" id="nav_buttons_Ventas">
+                                        <Tooltip title="App de busqueda" arrow>
+                                            <button onClick={() => setActive("buscar_Venta")} type="button" className="btn btn-primary">buscar</button>
+                                        </Tooltip>
+                                        <Tooltip title="App de edición" arrow>
+                                            <button onClick={() => setActive("actualizar_Venta")} type="button" className="btn btn-primary">actualizar</button>
+                                        </Tooltip>
 
-                            </nav>
+                                    </nav>
+                                </div>
+                            </div>
+                            <div className="row">
+                                {active === "buscar_Venta" && <h4>Buscar: </h4>}
+                                {active === "actualizar_Venta" && <h4>Actualizar: </h4>}
+                            </div>
+
                         </div>
                     </div>
-                    <div className="row">
-                        {active === "buscar_Venta" && <h4>Buscar: </h4>}
-                        {active === "actualizar_Venta" && <h4>Actualizar: </h4>}
-                    </div>
+
                 </div>
 
                 <div className="container">
-                    <div className="card" id="table_display_Ventas">
+                    <div className="card shadow-lg p-3 mb-5 bg-white rounded" id="table_display_Ventas">
                         <div className="card-body">
                             <div>
                                 {active === "buscar_Venta" && <TableVentaSearch></TableVentaSearch>}
